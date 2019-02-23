e5b383af02fea938106a680ae526048779a6881b
Labais <reinitto@gmail.com>
2019-02-23 17:16:11 +0100

    added css and fixed output ui

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-**FreeCodeCamp**- Information Security and Quality Assurance
-------
+## **FreeCodeCamp**- Information Security and Quality Assurance
 
-1) SET NODE_ENV to `test` without quotes
-2) Most logic will need done in `controllers/convertHandler.js` but do complete `routes/api.js`
-3) You will add any security features to `server.js`
-4) You will create all of the functional/unit tests in `tests/2_functional-tests.js` and `tests/1_unit-tests.js`
+1. **npm install** to install dependencies
+2. run **npm start** to start the server
+3. go to **localhost:3000**
 
+Testing
 
+1. SET NODE_ENV to `test` without quotes
--- routes/api.js
+++ routes/api.js
@@ -1,31 +1,43 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+"use strict";
+var ConvertHandler = require("../controllers/convertHandler.js");
 
-'use strict'
-var some;
-
-var expect = require('chai').expect;
-var ConvertHandler = require('../controllers/convertHandler.js');
-
-module.exports = function (app) {
-  
+module.exports = function(app) {
   var convertHandler = new ConvertHandler();
 
-  app.route('/api/convert')
-    .get(function (req, res){
-      var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
-      var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+  app.route("/api/convert").get(function(req, res) {
+    var errors = [];
+    var input = req.query.input;
+    if (!input) {
+      errors.push("No input, please input numbers and units");
+    }
+    var initNum = convertHandler.getNum(input);
+    if (typeof initNum !== "number") {
+      errors.push("Not a number");
+    }
 
-      res.json({initNum: initNum,initUnit: initUnit, returnNum: returnNum, returnUnit: returnUnit,String: toString})
-    });
-    
+    var initUnit = convertHandler.getUnit(input);
+    if (initUnit == "Invalid unit") {
+      errors.push("Invalid unit");
+    }
+    var returnNum = convertHandler.convert(initNum, initUnit);
+    var returnUnit = convertHandler.getReturnUnit(initUnit);
+    var toString = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+    console.log("errors length:", errors.length);
+    if (errors.length != 0) {
+      res.json(errors);
+    } else {
+      res.json({
+        initNum: initNum,
+        initUnit: initUnit,
+        returnNum: returnNum,
+        returnUnit: returnUnit,
+        string: toString
+      });
+    }
+  });
 };
